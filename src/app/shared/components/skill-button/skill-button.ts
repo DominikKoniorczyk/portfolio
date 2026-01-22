d9e2008f03d2bf055a0fed73a739365c1c56909a
Dominik Koniorczyk <d.koniorczyk@limesgames.de>
2026-01-22 17:34:37 +0100

    style technologie component

--- src/app/shared/components/skill-button/skill-button.ts
+++ src/app/shared/components/skill-button/skill-button.ts
@@ -10,15 +10,18 @@ import { TechnologiesSvg } from '../../services/technologies-svg';
 export class SkillButton {
   @Input() contentTemplate!: TemplateRef<any>;
   @Input() svgType: string = "";
-  @Input() svgWidth: number = 48;
-  @Input() svgHeight: number = 48;
-  svgPath: string = "";
+  @Input() svgWidth: string = "48px";
+  @Input() svgHeight: string = "48px";
+  svgPath: string = "M25.2 45.2984L38.136 41.7816L39.8804 22.8032H16.8728L16.3016 16.492H40.4544L41.09 10.3012H9.31L11.0908 28.9996H32.9868L32.256 37.0244L25.2 38.892L18.144 37.0244L17.7016 31.8612H11.3456L12.2668 41.7844L25.2 45.2984ZM0 0H50.4L45.8556 50.4L25.2 56L4.5444 50.4L0 0Z";
+  view : {x: number, y: number, w: number, h: number} = { x: 0, y: 0, w: 100, h: 100};
+  text: string = "HTML";
 
-  constructor(private svgPaths: TechnologiesSvg){}
-
-  ngAfterViewInit(){
+  constructor(private svgPaths: TechnologiesSvg){
     this.svgPath = this.svgPaths.returnPath(this.svgType);
+    this.view = this.svgPaths.returnViewBox(this.svgType);
   }
+
+  ngAfterViewInit(){}
 
   @HostBinding('style.--svg_width')
   get buttonWidth(){
@@ -28,5 +31,9 @@ export class SkillButton {
   @HostBinding('style.--svg_height')
   get buttonHeight(){
     return this.svgHeight;
+  }
+
+  get viewBox():string {
+    return `${this.view.x} ${this.view.y} ${this.view.w} ${this.view.h}`
   }
 }
--- src/app/shared/services/technologies-svg.ts
+++ src/app/shared/services/technologies-svg.ts
@@ -5,7 +5,7 @@ import { Injectable } from '@angular/core';
 })
 export class TechnologiesSvg {
 
-  html: string = "";
+  html: string = "M25.2 45.2984L38.136 41.7816L39.8804 22.8032H16.8728L16.3016 16.492H40.4544L41.09 10.3012H9.31L11.0908 28.9996H32.9868L32.256 37.0244L25.2 38.892L18.144 37.0244L17.7016 31.8612H11.3456L12.2668 41.7844L25.2 45.2984ZM0 0H50.4L45.8556 50.4L25.2 56L4.5444 50.4L0 0Z";
   css: string = "";
   js: string = "";
   ts: string = "";
@@ -14,6 +14,9 @@ export class TechnologiesSvg {
   git: string = "";
   api: string = "";
   mindset: string ="";
+
+  htmlSize: {x: number, y: number, w: number, h: number} = {x: 0, y: 0, w: 51, h: 56};
+  cssSize: {x: number, y: number, w: number, h: number} = {x: 0, y: 0, w: 51, h: 56};
 
   returnPath(input: string = "HTML"){
     switch(input){
@@ -27,6 +30,12 @@ export class TechnologiesSvg {
       case "API": return this.api;
       case "Mindset": return this.mindset;
       default: return this.html;
+    }
+  }
+
+  returnViewBox(input: string = "HTML"){
+    switch(input){
+      default: return this.htmlSize;
     }
   }
 }
