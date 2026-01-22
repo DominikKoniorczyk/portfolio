2c66adc1df58ee4d7d213a5104f1ab7fe38ef73e
Dominik Koniorczyk <d.koniorczyk@limesgames.de>
2026-01-22 18:22:39 +0100

    finish skill-svg service code

--- src/app/shared/components/skill-button/skill-button.ts
+++ src/app/shared/components/skill-button/skill-button.ts
@@ -1,4 +1,4 @@
-import { Component, HostBinding, Input, TemplateRef } from '@angular/core';
+import { Component, ElementRef, HostBinding, Input, TemplateRef, ViewChild } from '@angular/core';
 import { TechnologiesSvg } from '../../services/technologies-svg';
 
 @Component({
@@ -12,16 +12,38 @@ export class SkillButton {
   @Input() svgType: string = "";
   @Input() svgWidth: string = "48px";
   @Input() svgHeight: string = "48px";
-  svgPath: string = "M25.2 45.2984L38.136 41.7816L39.8804 22.8032H16.8728L16.3016 16.492H40.4544L41.09 10.3012H9.31L11.0908 28.9996H32.9868L32.256 37.0244L25.2 38.892L18.144 37.0244L17.7016 31.8612H11.3456L12.2668 41.7844L25.2 45.2984ZM0 0H50.4L45.8556 50.4L25.2 56L4.5444 50.4L0 0Z";
+  svgPath: string = "";
   view : {x: number, y: number, w: number, h: number} = { x: 0, y: 0, w: 100, h: 100};
   text: string = "HTML";
+  svgSource: string = "http://www.w3.org/2000/svg";
 
-  constructor(private svgPaths: TechnologiesSvg){
+  @ViewChild('svgContainer', { static: true }) svgContainer!: ElementRef<HTMLDivElement>;
+
+  constructor(private svgPaths: TechnologiesSvg){ }
+
+  ngAfterViewInit(){
     this.svgPath = this.svgPaths.returnPath(this.svgType);
     this.view = this.svgPaths.returnViewBox(this.svgType);
+    this.addSvg();
   }
 
-  ngAfterViewInit(){}
+  private addSvg(){
+    const svg = document.createElementNS(this.svgSource, 'svg');
+    svg.setAttribute('width', this.svgWidth);
+    svg.setAttribute('height', this.svgHeight);
+    svg.setAttribute('viewBox', this.viewBox);
+    this.generatePath(svg);
+  }
+
+  private generatePath(svg: Element){
+    const path = document.createElementNS(this.svgSource, 'path')
+    path.setAttribute('d', this.svgPath);
+    path.setAttribute('stroke', '#FFFFFF');
+    path.setAttribute('stroke-width', '1');
+    path.setAttribute('fill', '#FFFFFF');
+    svg.appendChild(path);
+    if(this.svgContainer) this.svgContainer.nativeElement.appendChild(svg);
+  }
 
   @HostBinding('style.--svg_width')
   get buttonWidth(){
