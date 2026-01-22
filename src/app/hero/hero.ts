78f519377909ad73051876187ed4c78b2f0e8075
Dominik Koniorczyk <d.koniorczyk@limesgames.de>
2026-01-22 09:21:51 +0100

    redesign hero banner for better optic. add imprint component

--- src/app/hero/hero.ts
+++ src/app/hero/hero.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { HeaderComp } from '../header-comp/header-comp';
 import { Title } from '../title/title';
 import { HeroBanner } from './../hero-banner/hero-banner';
+import { ScrollDown } from '../scroll-down/scroll-down';
 
 @Component({
   selector: 'app-hero',
-  imports: [HeaderComp, Title, HeroBanner],
+  imports: [HeaderComp, Title, HeroBanner, ScrollDown],
   templateUrl: './hero.html',
   styleUrl: './hero.scss',
 })
