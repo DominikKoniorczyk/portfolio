449d846adb2c205516235142ae03084e64902e5c
Dominik Koniorczyk <d.koniorczyk@limesgames.de>
2026-01-22 15:29:28 +0100

    add skill button

---
+++ src/app/shared/components/skill-button/skill-button.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-skill-button',
+  imports: [],
+  templateUrl: './skill-button.html',
+  styleUrl: './skill-button.scss',
+})
+export class SkillButton {
+
+}
