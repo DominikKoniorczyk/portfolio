38c67ef270e9d4d54e130067c17ec1247ea54023
Dominik Koniorczyk <d.koniorczyk@limesgames.de>
2026-01-22 14:24:01 +0100

    add technologies component to mainbody

--- src/app/shared/components/mainbody/mainbody.ts
+++ src/app/shared/components/mainbody/mainbody.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { Hero } from '../../../hero/hero';
 import { AboutMe } from '../../../about-me/about-me';
+import { Technologies } from '../../../technologies/technologies';
 
 @Component({
   selector: 'app-mainbody',
-  imports: [Hero, AboutMe],
+  imports: [Hero, AboutMe, Technologies],
   templateUrl: './mainbody.html',
   styleUrl: './mainbody.scss',
 })
