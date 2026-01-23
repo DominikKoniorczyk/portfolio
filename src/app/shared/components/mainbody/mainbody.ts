8aad7d1261c1c1fa6bba4b6536dfa1c54b38d8e0
Dominik Koniorczyk <d.koniorczyk@limesgames.de>
2026-01-23 14:10:04 +0100

    add projects code

--- src/app/shared/components/mainbody/mainbody.ts
+++ src/app/shared/components/mainbody/mainbody.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { Hero } from '../../../hero/hero';
 import { AboutMe } from '../../../about-me/about-me';
 import { Technologies } from '../../../technologies/technologies';
+import { Projects } from '../../../projects/projects';
 
 @Component({
   selector: 'app-mainbody',
-  imports: [Hero, AboutMe, Technologies],
+  imports: [Hero, AboutMe, Technologies, Projects],
   templateUrl: './mainbody.html',
   styleUrl: './mainbody.scss',
 })
