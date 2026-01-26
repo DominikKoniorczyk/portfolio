40fce246b7693b613a392bf4ebad18bfa3e9dc5f
Dominik Koniorczyk <d.koniorczyk@limesgames.de>
2026-01-26 12:44:53 +0100

    Add content to dialog

--- src/app/shared/components/mainbody/mainbody.ts
+++ src/app/shared/components/mainbody/mainbody.ts
@@ -3,10 +3,11 @@ import { Hero } from '../../../hero/hero';
 import { AboutMe } from '../../../about-me/about-me';
 import { Technologies } from '../../../technologies/technologies';
 import { Projects } from '../../../projects/projects';
+import { ProjectsDialog } from '../projects-dialog/projects-dialog';
 
 @Component({
   selector: 'app-mainbody',
-  imports: [Hero, AboutMe, Technologies, Projects],
+  imports: [Hero, AboutMe, Technologies, Projects, ProjectsDialog],
   templateUrl: './mainbody.html',
   styleUrl: './mainbody.scss',
 })
--- src/app/shared/components/projects-dialog/projects-dialog.ts
+++ src/app/shared/components/projects-dialog/projects-dialog.ts
@@ -1,4 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+
+export interface technology {
+  img: string;
+  name: string;
+}
 
 @Component({
   selector: 'app-projects-dialog',
@@ -7,5 +12,18 @@ import { Component } from '@angular/core';
   styleUrl: './projects-dialog.scss',
 })
 export class ProjectsDialog {
+  projectNumber: string = "01";
+  projectName: string = "El Pollo Loco";
+  projectDesc: string = "Jump, run and throw game based on object-oriented approach. Help Pepe to find coins and tabasco salsa to fight against the crazy hen."
+  projectTech?: technology[];
+  projectImage?: string;
 
+  openPage(pageToOpen: string){
+    switch (pageToOpen){
+      case ("El Pollo Loco"):
+      case ("Pokedex"):
+      case ("Join"):
+      default: return;
+    }
+  }
 }
