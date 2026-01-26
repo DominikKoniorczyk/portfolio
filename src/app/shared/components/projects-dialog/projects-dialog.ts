a8d13abe75897a771489152674684df3f7c5b566
Dominik Koniorczyk <d.koniorczyk@limesgames.de>
2026-01-26 09:59:39 +0100

    add dialog component

---
+++ src/app/shared/components/projects-dialog/projects-dialog.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-projects-dialog',
+  imports: [],
+  templateUrl: './projects-dialog.html',
+  styleUrl: './projects-dialog.scss',
+})
+export class ProjectsDialog {
+
+}
