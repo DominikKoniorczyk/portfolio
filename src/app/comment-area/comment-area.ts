0bbb1750c59d28c374260db9073d156a79b3e5fe
Dominik Koniorczyk <d.koniorczyk@limesgames.de>
2026-01-27 10:10:54 +0100

    add comment area component

---
+++ src/app/comment-area/comment-area.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-comment-area',
+  imports: [],
+  templateUrl: './comment-area.html',
+  styleUrl: './comment-area.scss',
+})
+export class CommentArea {
+
+}
