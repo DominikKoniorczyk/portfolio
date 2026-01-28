1f2b7bead58475d08bee5be717c47b77acf0fcf9
Dominik Koniorczyk <d.koniorczyk@limesgames.de>
2026-01-28 15:23:02 +0100

    fix problems in custom validators, add translation service on forms-controll, add en.json for i18n

--- src/app/shared/components/forms-controll/forms-controll.ts
+++ src/app/shared/components/forms-controll/forms-controll.ts
@@ -1,23 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { AnimatedButton } from '../animated-button/animated-button';
-import emailValidator, { CustomValidators } from '../../services/custom-validators';
+import emailValidator from '../../services/custom-validators';
+import { _, TranslatePipe, TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-forms-controll',
-  imports: [ReactiveFormsModule, AnimatedButton],
+  imports: [ReactiveFormsModule, AnimatedButton, TranslatePipe],
   templateUrl: './forms-controll.html',
   styleUrl: './forms-controll.scss',
 })
 export class FormsControll {
-  message = new FormControl('message');
+  translate = inject(TranslateService);
+  namePlaceHolder!: string;
+
+  constructor(translate: TranslateService){
+    translate.use('en');
+  }
+
+  ngOnInit(){
+    this.translate.get(['contact.formular.namePlaceholder', 'contact.formular.submit'])
+      .subscribe(translations => {
+        this.namePlaceHolder = translations['contact.formular.namePlaceholder'];
+      });
+  }
+
 
   userForm = new FormGroup({
-    name: new FormControl('name', { validators: [Validators.required, Validators.minLength(5)] }),
-    mail: new FormControl('email', { validators: [emailValidator() ] })
+    name: new FormControl('', { validators: [Validators.required, Validators.minLength(5)] }),
+    mail: new FormControl('', { validators: [emailValidator(), Validators.required] }),
+    message: new FormControl('', { validators: [Validators.required, Validators.minLength(30)] }),
+    policy: new FormControl('', { validators: [Validators.required] })
   });
 
-  formSubmit(){
+  onNameInputFocus() {
+
+  }
+
+  formSubmit() {
     console.log(this.userForm.get('mail')?.valid);
   }
 }
