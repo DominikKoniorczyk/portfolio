862c24f2318e183bfad5fa4bc737be27754b6cc6
Dominik Koniorczyk <d.koniorczyk@limesgames.de>
2026-01-29 14:58:22 +0100

    add translation pipes to header and hero component html

--- src/app/header-comp/header-comp.ts
+++ src/app/header-comp/header-comp.ts
@@ -1,10 +1,11 @@
 import { Router } from '@angular/router';
 import { ScrollService } from '../shared/services/scroll.service';
 import { Component, inject } from '@angular/core';
+import { TranslatePipe } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-header-comp',
-  imports: [],
+  imports: [TranslatePipe],
   templateUrl: './header-comp.html',
   styleUrl: './header-comp.scss',
 })
--- src/app/hero/hero.ts
+++ src/app/hero/hero.ts
@@ -3,10 +3,11 @@ import { HeaderComp } from '../header-comp/header-comp';
 import { Title } from '../title/title';
 import { HeroBanner } from './../hero-banner/hero-banner';
 import { ScrollDown } from '../scroll-down/scroll-down';
+import { TranslatePipe } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-hero',
-  imports: [HeaderComp, Title, HeroBanner, ScrollDown],
+  imports: [HeaderComp, Title, HeroBanner, ScrollDown, TranslatePipe],
   templateUrl: './hero.html',
   styleUrl: './hero.scss',
 })
