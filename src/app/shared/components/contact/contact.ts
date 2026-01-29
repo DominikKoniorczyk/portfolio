c44761bbeafd661afb2741545b7fcc019591752c
Dominik Koniorczyk <d.koniorczyk@limesgames.de>
2026-01-29 08:53:37 +0100

    add translation tags to contact

--- src/app/shared/components/contact/contact.ts
+++ src/app/shared/components/contact/contact.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { FormsControll } from '../forms-controll/forms-controll';
+import { TranslatePipe } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-contact',
-  imports: [FormsControll],
+  imports: [FormsControll, TranslatePipe],
   templateUrl: './contact.html',
   styleUrl: './contact.scss',
 })
