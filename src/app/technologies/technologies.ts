e4d18f7b2cc2e4883772e5314d6b0a39ae043696
Dominik Koniorczyk <d.koniorczyk@limesgames.de>
2026-01-29 15:40:10 +0100

    add translations to technologies

--- src/app/technologies/technologies.ts
+++ src/app/technologies/technologies.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { AnimatedButton } from '../shared/components/animated-button/animated-button';
 import { ScrollService } from '../shared/services/scroll.service';
 import { SkillButton } from '../shared/components/skill-button/skill-button';
+import { TranslatePipe } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-technologies',
-  imports: [AnimatedButton, SkillButton],
+  imports: [AnimatedButton, SkillButton, TranslatePipe],
   templateUrl: './technologies.html',
   styleUrl: './technologies.scss',
 })
