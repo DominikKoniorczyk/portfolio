5d8c757023aad47cdfb5ed2911f734fb20cd52b7
Dominik Koniorczyk <d.koniorczyk@limesgames.de>
2026-02-02 11:02:42 +0100

    add error to getElementById

--- src/app/shared/services/scroll.service.ts
+++ src/app/shared/services/scroll.service.ts
@@ -27,6 +27,7 @@ export class ScrollService {
    */
   private getElementById(id: string): HTMLElement {
     const element = document.getElementById(id);
+    if(!element) throw new Error('Element with ID "${id}" not found.');
     return (element!);
   }
 
